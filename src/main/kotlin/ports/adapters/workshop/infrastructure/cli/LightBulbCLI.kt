d15b577f099a4fc5e640261b62d6ac8061bbb2e9
David Gracia Celemendi <david.gracia.celemendi@gmail.com>
2020-01-07 21:24:42 +0100

    control light bulb via CLI

---
+++ src/main/kotlin/ports/adapters/workshop/infrastructure/cli/LightBulbCLI.kt
@@ -0,0 +1,22 @@
+package ports.adapters.workshop.infrastructure.cli
+
+import org.springframework.stereotype.Component
+import ports.adapters.workshop.interaction.TurnOffLightBulb
+import ports.adapters.workshop.interaction.TurnOnLightBulb
+
+@Component
+class LightBulbCLI(val turnOnLightBulb: TurnOnLightBulb, val turnOffLightBulb: TurnOffLightBulb) {
+    fun repl() {
+        var line: String? = readLine()
+        while (line != null) {
+            // This variable declaration is a simplification
+            val id = 1
+
+            when (line) {
+                "turn on" -> turnOnLightBulb.execute(id)
+                "turn off" -> turnOffLightBulb.execute(id)
+            }
+            line = readLine()
+        }
+    }
+}
